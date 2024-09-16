40cdeef666a15241db28212ff234678cd32aea7b
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-17 00:04:47 +0300

    added nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,42 @@
-// NavBar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const NavBar = ({ toggleTheme, searchQuery, setSearchQuery, fetchMoviesBySearch }) => {
+const NavBar = ({ toggleTheme }) => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+
   return (
-    <div className="mainnav">
-      <nav className="navbar">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/series">Series</NavLink>
-        <NavLink to="/episodes">Episodes</NavLink>
-        <NavLink to="/library">Library</NavLink>
-        <NavLink to="/add-movie"><button className="addbutton">Add Movie</button></NavLink>
-      </nav>
-      <button onClick={toggleTheme}>
-        Toggle Theme
+    <div>
+      {/* Hamburger Button */}
+      <button className={`hamburger ${isNavVisible ? 'active' : ''}`} onClick={toggleNavbar}>
+        <div></div>
+        <div></div>
+        <div></div>
       </button>
+
+      {/* Navbar Main */}
+      <div className={`mainnav ${isNavVisible ? 'navbar-visible' : 'navbar-hidden'}`}>
+        <nav className="navbar">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/series">Series</NavLink>
+          <NavLink to="/episodes">Episodes</NavLink>
+          <NavLink to="/library">Library</NavLink>
+        </nav>
+        
+        {/* Navbar Controls */}
+        <div className="navbar-controls">
+          <NavLink to="/add-movie">
+            <button className="addbutton">Add Movie</button>
+          </NavLink>
+          <button onClick={toggleTheme}>
+            Toggle Theme
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
